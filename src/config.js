54dc065466bc666063149d5c3bf3ae1f437b65c0
Zain Ul Abidin <zchxvr@gmail.com>
2024-09-30 20:28:45 +0300

    project

---
+++ src/config.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+// axios.defaults.baseURL = 'https://new-admin.nowtradefunded.com';
+// axios.defaults.baseURL = "http://backend.nowtradefunded.com";
+// axios.defaults.baseURL = "https://avery-admin-backend.onrender.com";
+// axios.defaults.baseURL = "http://localhost:8001";
+axios.defaults.baseURL = "https://bp8jxqx4-8001.usw3.devtunnels.ms/";
